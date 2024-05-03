8e7b19b23c1d620571c4f80e4eecbaac09bc358e
mustafa1728 <mustchasmai@gmail.com>
2024-05-03 11:09:34 -0400

    update pixel accuracy defn

--- experiments/evaluate.py
+++ experiments/evaluate.py
@@ -13,7 +13,8 @@ class SemSegMetrics():
 
     def update(self, pred, gt):
         self.count += 1
-        self.metrics["pixel_acc"] += (pred == gt).sum()/(gt.shape[0] * gt.shape[1])
+        # self.metrics["pixel_acc"] += (pred == gt).sum()/(gt.shape[0] * gt.shape[1])
+        self.metrics["pixel_acc"] += (((pred == 1) * (gt == 1)).sum()/(gt == 1).sum() + ((pred == 0) * (gt == 0)).sum()/(gt == 0).sum())/2
         self.metrics["dice"] += 2 * (pred * gt).sum()/(pred.sum() + gt.sum())
         self.metrics["miou"] += (pred * gt).sum()/((pred + gt)>0).sum()
 
@@ -29,15 +30,20 @@ class SemSegMetrics():
 
 def eval_segmentation(result_folder, gt_folder):
     metrics = SemSegMetrics()
-    for fname in tqdm.tqdm(os.listdir(result_folder)):
+    for fname in tqdm.tqdm(os.listdir(result_folder), leave=False):
         pred_path = os.path.join(result_folder, fname)
-        gt_path = os.path.join(gt_folder, fname)
-        if ".jpg" in gt_path:
-            gt_path = gt_path.replace(".jpg", ".png")
-
+        gt_path = os.path.join(gt_folder, fname.replace(".jpg", ".png"))
+        if not os.path.exists(pred_path) or not os.path.exists(gt_path):
+            continue
         pred = cv2.imread(pred_path, 0)
-        gt   = cv2.imread(gt_path, 0)
-        metrics.update(pred>0, gt>0)
+        gt     = cv2.imread(gt_path, 0)
+        if pred.shape[0] != gt.shape[0] or pred.shape[1] != gt.shape[1]:
+            pred = cv2.resize(pred, (gt.shape[1], gt.shape[0]))
+            
+        pred = (pred -    pred.min()) / (pred.max() - pred.min() + 1e-10)
+        gt = (gt -    gt.min()) / (gt.max() - gt.min() + 1e-10)
+        metrics.update(pred>0.5, gt>0.5)
 
     print(metrics)
     return metrics
+
